5284183fa6623d662bb86596878b0b1241ea072c
dominisz <dominisz@gmail.com>
2018-02-13 20:28:58 +0100

    Polskie znaki, welcome file

--- src/main/java/pl/dominisz/TimeServlet.java
+++ src/main/java/pl/dominisz/TimeServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -13,9 +14,15 @@ public class TimeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Dzisiaj jest " + LocalDate.now() + "</h1>");
+        LocalDate today = LocalDate.now();
+        out.println("<h1>Dzisiaj jest " + today + "</h1>");
         out.println("<h1>Godzina " + LocalTime.now() + "</h1>");
+        if (today.getDayOfWeek() == DayOfWeek.TUESDAY) {
+            out.println("<h1>Hurra!!! Wtorek!!!</h1>");
+        }
+        out.println("<h1>Zażółć gęślą jaźń</h1>");
     }
 
 }
